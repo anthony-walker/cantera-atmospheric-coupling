74728ef6a6e4879a08f5cb4ee3a89e1dcf930d9a
Anthony Walker <walkerant@kairospower.com>
2024-01-28 21:24:21 -0500

    Add statement to run only missing jobs

--- study/cstudy.py
+++ study/cstudy.py
@@ -70,15 +70,20 @@ def run_combustion_study(run_missing=True):
     with Pool(numpy.amin([os.cpu_count(), len(vals_list)])) as p:
         p.map(wrapped, vals_list)
 
-def make_species_contour(unformatted, sp, index=-1, path="fullmcm", name=""):
-    equiv_ratios = numpy.arange(0.5, 3.25, 0.25)
+def make_species_contour(sp, index=-1, path="fullmcm", name="", mode="hdf5", scale="short"):
+    equiv_ratios = numpy.arange(0.6, 2.6, 0.1)
     farnesane_percents = numpy.arange(0, 0.21, 0.01)
+    unformatted = f"{scale}-term-states-"+ "{:.1f}-{:.2f}" + f".{mode}"
     masses = numpy.zeros((len(farnesane_percents), len(equiv_ratios)))
     for i, eq in enumerate(equiv_ratios):
         for j, fs in enumerate(farnesane_percents):
             if os.path.exists(os.path.join(path, unformatted.format(eq, fs))):
-                short_data = pd.read_csv(os.path.join(path, unformatted.format(eq, fs)), delimiter=",", engine="python", usecols=["mass", "time", f'Y_{sp}'])
-                masses[j][i] = short_data[f'Y_{sp}'].iloc[index] * short_data["mass"].iloc[index]
+                if mode == "csv":
+                    short_data = pd.read_csv(os.path.join(path, unformatted.format(eq, fs)), delimiter=",", engine="python", usecols=["mass", "time", f'Y_{sp}'])
+                    masses[j][i] = short_data[f'Y_{sp}'].iloc[index]
+                elif mode == "hdf5":
+                    short_data = h5py.File(os.path.join(path, unformatted.format(eq, fs)), "r")
+                    masses[j][i] = short_data[f'Y_{sp}'][index]
             else:
                 masses[j][i] = numpy.NAN
     # make contour
@@ -90,7 +95,7 @@ def make_species_contour(unformatted, sp, index=-1, path="fullmcm", name=""):
     plt.colorbar(label='Mass [kg]')
     if not os.path.exists("figures"):
         os.mkdir("figures")
-    plt.savefig(f"figures/{name}-contour.pdf")
+    plt.savefig(f"figures/{name}-{scale}-contour.pdf")
     plt.close()
 
 def short_term_states_plots(species, path="fullmcm"):
@@ -104,29 +109,34 @@ def short_term_states_plots(species, path="fullmcm"):
     # ax.plot(time, mass)
     plt.show()
 
-def long_term_states_plots(species, path="fullmcm"):
-    short_data = pd.read_csv(os.path.join(path, 'long-term-states-1.5-0.10.csv'), delimiter=",", engine="python")
-    mass = short_data["mass"]
-    time = short_data["time"]
-    Y_sp = short_data[f"Y_{species}"]
-    short_data.keys()
-    for k in short_data.keys():
+def long_term_states_plots(species, path="fullmcm", mode="csv", scale="long"):
+    if mode == "csv":
+        short_data = pd.read_csv(os.path.join(path, f'{scale}-term-states-1.5-0.10.csv'), delimiter=",", engine="python")
+        mass = short_data["mass"]
+        time = short_data["time"]
+    elif mode == "hdf5":
+        short_data = h5py.File(os.path.join(path, f'{scale}-term-states-1.5-0.09.hdf5'), "r")
+        mass = numpy.array(short_data["mass"])
+        time = numpy.array(short_data["time"])
+    if species is None:
+        for k in short_data.keys():
+            if (numpy.mean(mass * numpy.array(short_data[k])) > 1e-30):
+                fig, ax = plt.subplots()
+                ax.plot(time, mass * numpy.array(short_data[k]))
+                ax.set_title(k)
+                plt.savefig(f"figures/{k}-{scale}.pdf")
+                plt.close()
+    else:
         fig, ax = plt.subplots()
-        # ax.plot(time, mass * Y_tol)
-        ax.plot(time, mass * short_data[k])
-        ax.set_title(k)
-        # ax.plot(time, mass)
-        plt.savefig(f"figures/{k}.pdf")
+        ax.plot(time, mass * numpy.array(short_data[f"Y_{species}"]))
+        plt.savefig(f"figures/{species}-{scale}.pdf")
         plt.close()
 
 
-def plot_all():
-    make_species_contour("short-term-states-{:.1f}-{:.2f}.csv", "TOLUENE", name="toluene-short-0", index=0)
-    make_species_contour("short-term-states-{:.1f}-{:.2f}.csv", "BENZENE", name="benzene-short-0", index=0)
-    make_species_contour("short-term-states-{:.1f}-{:.2f}.csv", "C5H8", name="isoprene-short-0", index=0)
-
 if __name__ == "__main__":
+    mode = "hdf5"
+    path = "minimal"
     # run_combustion_study()
-    long_term_states_plots("O2")
-    # short_term_states_plots("BENZENE")
-    # short_term_states_plots("C5H8")
+    # long_term_states_plots(None, path=path, mode=mode)
+    # long_term_states_plots(None, path=path, mode=mode, scale="short")
+    make_species_contour("TOLUENE", name="toluene-short-0", index=0, path=path, mode=mode)
